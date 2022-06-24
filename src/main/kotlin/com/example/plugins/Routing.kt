bccc9c4346a56964348543b42960b528e610816e
jinapark <jina940323@gmail.com>
2022-06-24 13:56:47 +0900

    ktor basic structure

---
+++ src/main/kotlin/com/example/plugins/Routing.kt
@@ -0,0 +1,19 @@
+package com.example.plugins
+
+import com.example.routes.customerRouting
+import com.example.routes.listOfOrders
+import io.ktor.server.routing.*
+import io.ktor.server.application.*
+import io.ktor.server.response.*
+
+fun Application.configureRouting() {
+
+    routing {
+        get("/") {
+            call.respondText("Hello World!")
+        }
+        customerRouting()
+        listOfOrders()
+    }
+
+}
